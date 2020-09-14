41b99b72129abc33c9264d11b9632e1f42f7cfa9
sylvain <sylvain.krieg@ecorobotix.com>
2020-09-14 23:06:30 +0200

    [Software][application] include gui and halisimu

--- software/modules/application/src/main.cpp
+++ software/modules/application/src/main.cpp
@@ -1,7 +1,21 @@
 #include <iostream>
+#include "src/webInterface/web_ws.hpp"
+#include <thread>
 #include "src/webInterface/messages_handler.h"
-
+#include "Hali.h"
+#include "HalSimu.h"
 int main() {
-    std::cout<<"application"<<std::endl;
+    Hali* hali;
+    auto realhali = new HalSimu();
+    hali = realhali;
+    std::cout << "md25 voltage  : " << hali->getBatteryVoltage() << std::endl;
+    return 0;
+
+    messages_handler mh;
+
+    //run web interface
+    std::thread websockServer(start_server, mh);
+
+    websockServer.join();
     return 0;
 }
