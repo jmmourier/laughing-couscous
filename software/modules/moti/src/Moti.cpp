2a5f6b7aae3f3ca5af0417577c20edf27963bff8
jmmourier <jm.mourier.11@eigsi.fr>
2020-09-23 23:55:25 +0200

    [Software][moti] add struct and main method to moti module

--- software/modules/moti/src/Moti.cpp
+++ software/modules/moti/src/Moti.cpp
@@ -1,5 +1,19 @@
 #include "Moti.h"
 
-Moti::Moti(/* args */)
-{
+Moti::Moti(){}
+
+MotorSpeed Moti::getMotorSpeedFromBaseSpeed(BaseSpeed base_speed){
+    //TODO
+}
+
+void Moti::setWheelDiameter(float wheel_diameter_mm){
+    wheel_diameter_mm_ = wheel_diameter_mm;
+}
+
+void Moti::setWheelDistance(float wheel_distance_mm){
+    wheel_distance_mm_ = wheel_distance_mm;
+}
+
+void Moti::setMaxWheelSpeed(float max_wheel_speed_radps){
+    max_wheel_speed_radps_ = max_wheel_speed_radps;
 }
