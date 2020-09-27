d590a8b8c1ffc822085208fa0e01dce2d7e138bc
Didier Capozzi <didier.capozzi@ecorobotix.com>
2020-09-27 12:54:34 +0000

    [posi module] add robot consts

--- software/modules/posi/src/Posi.cpp
+++ software/modules/posi/src/Posi.cpp
@@ -1,4 +1,10 @@
 #include "Posi.h"
+#include <cmath>
+
+const int TICKS_PER_ROTATION = 360;
+const double SPACE_BETWEEN_WHEELS = 0.2;
+const double WHEEL_RADIUS_M = 0.1;
+const double WHEEL_PERIMETER = M_PI * 2 * WHEEL_RADIUS_M;
 
 Posi::Posi(double &start_pos_x, double &start_pos_y, double &start_orientation)
     : abs_pos_x_(start_pos_x), abs_pos_y_(start_pos_y),
