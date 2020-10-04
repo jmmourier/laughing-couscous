166dab1c27b45f57d7e93fe4a07e71665874836f
Didier Capozzi <capozzi.didier@gmail.com>
2020-10-04 14:58:31 +0000

    [app test gui] main now launch couscous manager

--- software/application_test_gui/src/main.cpp
+++ software/application_test_gui/src/main.cpp
@@ -1,1 +1,14 @@
-int main(int argc, char *argv[]) { return 0; }
+#include "CouscousManager.h"
+#include "Posi.h"
+#include "RealTime.h"
+#include "WebServer.h"
+#include <iostream>
+#include <memory>
+
+int main(int argc, char *argv[]) {
+
+  auto couscousManager = std::make_shared<CouscousManager>();
+  couscousManager->start();
+
+  return 0;
+}
