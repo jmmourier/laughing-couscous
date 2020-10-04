0da2c59d373045f45e79f83b982104e06a5047d0
Didier Capozzi <capozzi.didier@gmail.com>
2020-10-04 14:54:55 +0000

    [gui] add new grpc position service to web server

--- software/modules/gui/src/WebServer.cpp
+++ software/modules/gui/src/WebServer.cpp
@@ -1,13 +1,16 @@
 
-#include "../include/WebServer.h"
+#include "WebServer.h"
+
+#include "WebPositionService.h"
 #include "grpcpp/ext/proto_server_reflection_plugin.h"
-#include "grpcpp/health_check_service_interface.h"
-#include "grpcpp/security/server_credentials.h"
 #include "grpcpp/server_builder.h"
-#include <string>
+
+WebServer::WebServer(
+    std::function<void(double pos_x_m, double pos_y_m, double orientation_rad)>
+        on_set_position_callback)
+    : web_position_service_(std::move(on_set_position_callback)) {}
 
 void WebServer::start() {
-
   std::string server_address("0.0.0.0:9090");
 
   grpc::EnableDefaultHealthCheckService(true);
@@ -17,9 +20,16 @@ void WebServer::start() {
 
   grpc_builder.AddListeningPort(server_address,
                                 grpc::InsecureServerCredentials());
+  grpc_builder.RegisterService(&web_position_service_);
 
-  std::unique_ptr<grpc::Server> server(grpc_builder.BuildAndStart());
+  server_ = grpc_builder.BuildAndStart();
   std::cout << "Server listening on " << server_address << std::endl;
-  server->Wait();
+  server_->Wait();
 }
-void WebServer::stop() {}
+
+void WebServer::updatePosition(double pos_x_m, double pos_y_m,
+                               double orientation_rad) {
+  web_position_service_.updatePosition(pos_x_m, pos_y_m, orientation_rad);
+}
+
+void WebServer::stop() { server_->Shutdown(); }
