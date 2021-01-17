6bf87c503e13a1ada0cc53035102a3676bfcd568
jmmourier <jm.mourier.11@eigsi.fr>
2021-01-17 23:35:33 +0100

    [Soft][hali] change serial lib to embbeded instead of boost::asio

--- software/modules/hali/src/Serial.cpp
+++ software/modules/hali/src/Serial.cpp
@@ -1,22 +1,23 @@
 #include "Serial.h"
 
-#include <boost/asio.hpp>
+#include <serialib.h>
 #include <iostream>
 
-Serial::Serial(std::string port, unsigned int baud_rate) : io(), serial(io, port) {
-    serial.set_option(boost::asio::serial_port_base::baud_rate(baud_rate));
+Serial::Serial(std::string port, unsigned int baud_rate) {
+     char errorOpening = serial.openDevice(port.c_str(), baud_rate);
+
+    if (errorOpening!=1) throw "[hali] Port can not be open";
 }
 
-void Serial::writeString(std::string s) {
-    std::cout << "writing : " << s << std::endl;
-    boost::asio::write(serial, boost::asio::buffer(s.c_str(), s.size()));
+void Serial::writeString(std::string string_input) {
+    serial.writeString(string_input.c_str());
 }
 
 std::string Serial::readLine() {
     char c;
     std::string result;
     for (;;) {
-        boost::asio::read(serial, boost::asio::buffer(&c, 1));
+        serial.readChar(&c);
         switch (c) {
             case '\r':
                 break;
@@ -31,7 +32,7 @@ std::string Serial::readLine() {
 char Serial::readChar() {
     char c;
     try {
-        boost::asio::read(serial, boost::asio::buffer(&c, 1));
+        serial.readChar(&c);
     } catch (const std::exception &e) {
         // std::cout << e.what() << std::endl;
         return 0;
