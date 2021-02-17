83c15f5102b2960ef597048433a070c308872fe6
jmmourier <jm.mourier.11@eigsi.fr>
2021-02-17 19:20:13 +0100

    [Soft][missi] add json lib and implement mission from file

--- software/modules/missi/include/Missi.h
+++ software/modules/missi/include/Missi.h
@@ -2,25 +2,27 @@
 #define MISSI_H
 
 #include <string>
-
+#include <vector>
 #include "Action.h"
-
-enum MissionColor { BLUE, YELLOW };
 
 class Missi {
    private:
-    void loadMissionFile();
-    std::string mission_file_path;
-    MissionColor mission_color_;
+    std::string mission_file_path_;
     bool previous_action_has_been_done_;
 
-    Action current_action;
+    Action current_action_;
+    int next_action_indice_;
+    std::vector<Action> action_list_;
+
+    ActionType stringToActionType(std::string action_type_as_string);
+    std::string actionTypeToString(ActionType action_type);
 
    public:
     Missi();
-    void setMissionColor(MissionColor mission_color);
+    void loadMissionFile();
+    void setMissionFilePath(std::string mission_file_path);
 
-    void actionHasBeenDone(bool action_done);
+    void actionHasBeenDone();
     Action getCurrentAction();
 };
 
