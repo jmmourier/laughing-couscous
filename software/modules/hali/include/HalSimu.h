9d752bfa40c9db35630f476ecfff44b73495938d
jean-michel.mourier <jean-michel.mourier@ecorobotix.com>
2021-03-30 23:28:51 +0200

    [Soft][hali] update simu zero value for motor control and ratio speed time

--- software/modules/hali/include/HalSimu.h
+++ software/modules/hali/include/HalSimu.h
@@ -30,8 +30,8 @@ class HalSimu : public IHali {
 
     GrabberState grabber_state_;
 
-    int saved_speed_1_ = 127;
-    int saved_speed_2_ = 127;
+    int saved_speed_1_ = 128;
+    int saved_speed_2_ = 128;
 
     std::chrono::_V2::system_clock::time_point timestamp_since_last_encoder_1_update_;
     std::chrono::_V2::system_clock::time_point timestamp_since_last_encoder_2_update_;
@@ -39,7 +39,9 @@ class HalSimu : public IHali {
     int encoder_1_ = 0;
     int encoder_2_ = 0;
 
-    const float ratio_speed_time_ = 0.001;
+    // 1/127 (range of speed) * 170 (tr/min max) * 360 (ticks per revolution) *
+    // 1/60 (for seconds) * 1/1000 (for milliseconds)  
+    const float ratio_speed_time_ = 0.00803;
 };
 
 #endif  // HAL_SIMU_H
