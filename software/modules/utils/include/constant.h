ccd954cb8f122166dccbd47cae017b9c11967d0e
Didier Capozzi <didier.capozzi@ecorobotix.com>
2021-04-09 15:11:45 +0000

    [posi] fix position to absolute between 0 and 2 * PI

--- software/modules/utils/include/constant.h
+++ software/modules/utils/include/constant.h
@@ -1,10 +1,15 @@
 #ifndef CONSTANT_H
 #define CONSTANT_H
 
-namespace constant {
-    static const float wheel_diameter_m_ = 0.100;
-    static const float wheel_distance_m_ = 0.253;
-    static const float max_wheel_speed_radps_ = 17.802;  // 170 rpm <> 17,802 radps
-}
+#include <cmath>
 
-#endif // CONSTANT_H
+namespace constant {
+static const float wheel_diameter_m_ = 0.100;
+static const float wheel_distance_m_ = 0.253;
+static const float max_wheel_speed_radps_ = 17.802;  // 170 rpm <> 17,802 radps
+static const int ticks_per_rotation = 360;
+static const float whole_rotation_rad = M_PI * 2;
+static const float wheel_perimeter_m = whole_rotation_rad * wheel_diameter_m_ / 2;
+}  // namespace constant
+
+#endif  // CONSTANT_H
