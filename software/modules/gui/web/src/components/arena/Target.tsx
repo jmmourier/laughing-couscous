19022e56966d8bc8117e25cafb7396a7648de277
Didier Capozzi <didier.capozzi@ecorobotix.com>
2021-04-28 18:46:43 +0000

    [UI front] fix target position

--- software/modules/gui/web/src/components/arena/Target.tsx
+++ software/modules/gui/web/src/components/arena/Target.tsx
@@ -1,11 +1,12 @@
 import React, { FunctionComponent, useContext } from "react";
 import { context, IPosition } from "../StateProvider";
+import { ARENA_HEIGHT } from "./Arena";
 
 const Target: FunctionComponent<IPosition> = ({ x_m, y_m }) => {
   const stateContext = useContext(context);
 
   return (
-    <g transform={`translate(${x_m},${y_m})`}>
+    <g transform={`translate(${x_m},${ARENA_HEIGHT - y_m})`}>
       <circle
         style={{ fill: "red" }}
         r={0.05}
